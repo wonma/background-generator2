e53e91a8aeec6fe9cd337ef4b7cd9c7ff5d186c8
Wonmi Kwon <42050917+wonma@users.noreply.github.com>
2022-06-14 21:47:08 -0500

    Added color input fields

---
+++ script.js
@@ -0,0 +1,8 @@
+var body = document.querySelector('body');
+var colorPicker1 = document.querySelector('#favColor1');
+var colorPicker2 = document.querySelector('#favColor2');
+
+colorPicker1.addEventListener('input', e => {
+  var changedColor = e.target.value;
+  body.style.
+});
